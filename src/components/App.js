864fe2160dd8e81b3d687fecd01f05bb2fb9ebe6
tahirbhalli <tahirbhalli16@gmail.com>
2020-08-21 20:50:20 +0500

    Revert "start styling"
    This reverts commit 4fbfd0345aa85685e82b65841ba6e8a0682cf957.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
-import Header from './Header';
 
 export default function App() {
   return (
     <div>
-      <Header />
       <BooksForm />
       <BooksList />
     </div>
