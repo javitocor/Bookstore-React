0fa18d18d9809cd2c0a57361fa7f81c6b6c517de
javitocor <56809963+javitocor@users.noreply.github.com>
2020-08-21 14:27:22 +0200

    Modify booksform into a class

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,17 +1,30 @@
 /* eslint-disable max-len */
 import React from 'react';
+import { generateId } from '../sampleBooks';
 
 const BookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 const dropMenu = BookCategories.map(category => <option key={category} value={category}>{category}</option>);
 
-const BooksForm = () => (
-  <form>
+class BooksForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      id: generateId(),
+      title: '',
+      category= BookCategories[0],
+    }
+  }
+  render(){
+    return (
+      <form>
     <input className="" type="text" placeholder="Enter the Title" id="title" name="title" />
     <select>
       {dropMenu}
     </select>
     <button type="submit">Add Book</button>
   </form>
-);
+    )
+  }
+}
 
 export default BooksForm;
