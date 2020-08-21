a67982e1383eae024eb7e70305d91710edf70f71
javitocor <56809963+javitocor@users.noreply.github.com>
2020-08-21 12:09:58 +0200

    create combine reducers file

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import { createStore } from 'redux';
 import booksReducer from './reducers/books';
 import './index.css';
 import App from './components/App';
+import sampleBooks from './sampleBooks';
 
-const store = createStore(booksReducer);
+const store = createStore(booksReducer, sampleBooks);
 
 ReactDOM.render(
   <Provider store={store}>
