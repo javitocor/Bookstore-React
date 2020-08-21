4fbfd0345aa85685e82b65841ba6e8a0682cf957
tahirbhalli <tahirbhalli16@gmail.com>
2020-08-21 20:48:43 +0500

    start styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
+import Header from './Header';
 
 export default function App() {
   return (
     <div>
+      <Header />
       <BooksForm />
       <BooksList />
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="panel-bg">
+      <h2 className="Bookstore-CMS">Bookstore-CMS</h2>
+      <p className="BOOKS">Books</p>
+    </div>
+  );
+}
